0aae45182619153dbec2be409e7d13413a9f7599
Cristian Ion <cristi.ion21@gmail.com>
2018-02-05 10:21:56 -0500

    first commit for test

---
+++ src/HelloWorldClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Cristian Ion
+ * I'm testing if the projects works as a Java project, since I've imported from a git repository
+ *
+ */
+public class HelloWorldClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is just a test");
+	}
+
+}
